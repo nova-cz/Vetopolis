ea15ebe486bcf883f89e17dcac222565b97eaebe
Miguel_CR <125812157+nova-cz@users.noreply.github.com>
2025-04-29 00:22:38 -0600

    fix: move build to docs and set correct base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/Vetopolis/', // 👈 Agregado para GitHub Pages
+  base: '/Vetopolis/', 
   server: {
     host: "::",
     port: 8080,
