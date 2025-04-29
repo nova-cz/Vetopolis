f42e52bc26fcb4ac75c3e952c704d904cd41ab02
Miguel_CR <125812157+nova-cz@users.noreply.github.com>
2025-04-29 00:12:40 -0600

    Updates

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Vetopolis/', // 👈 Agregado para GitHub Pages
   server: {
     host: "::",
     port: 8080,
